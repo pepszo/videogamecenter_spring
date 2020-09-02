0d699abdd64f0dc5523f5156a8ddb9d2eeee892f
Pepszo <memodebouvy@hotmail.com>
2020-09-02 05:45:34 +0200

    inital commit

---
+++ src/main/java/tfe/videogamecenter/VideogamecenterApplication.java
@@ -0,0 +1,13 @@
+package tfe.videogamecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogamecenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideogamecenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tfe/videogamecenter/VideogamecenterApplicationTests.java
@@ -0,0 +1,13 @@
+package tfe.videogamecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideogamecenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
